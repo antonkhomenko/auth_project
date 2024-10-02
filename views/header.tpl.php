b3a8c43e159b3859a60325a4592e042bc07fe8fe
antonkhomenko <antonkhomenko5@gmail.com>
2024-10-02 22:55:46 +0300

    login form template

--- views/header.tpl.php
+++ views/header.tpl.php
@@ -22,7 +22,7 @@ $default_mode = $_COOKIE['mode'] ?? 'dark';
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="/assets/imgs/favicon.svg">
 </head>
-<body class="d-flex flex-column" style="min-height: 100vh">
+<body class="d-flex flex-column bg-body-secondary" style="min-height: 100vh">
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
 	<div class="container-lg">
 		<a class="navbar-brand fw-semibold d-flex" href="/">
@@ -49,7 +49,7 @@ $default_mode = $_COOKIE['mode'] ?? 'dark';
                        aria-expanded="false">
                         User
                     </a>
-                    <ul class="dropdown-menu">
+                    <ul class="dropdown-menu" style="left: -50%">
                         <li><a class="dropdown-item" href="#">Profile</a></li>
                         <li>
                             <hr class="dropdown-divider">
--- views/login.tpl.php
+++ views/login.tpl.php
@@ -1,8 +1,27 @@
 <?php include VIEWS . "/header.tpl.php"; ?>
 
 <div class="flex-grow-1">
-	<div class="container">
-		<h1>Login page</h1>
+	<div class="container-lg">
+        <form class="col-lg-6 offset-lg-3 py-4">
+            <h1 class="py-3 fw-semibold">Login in account</h1>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3 form-check d-flex justify-content-between">
+                <div>
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <a href="/register">Do not have an account ?</a>
+            </div>
+            <button type="submit" class="btn btn-primary">Log in</button>
+        </form>
 	</div>
 </div>
 
